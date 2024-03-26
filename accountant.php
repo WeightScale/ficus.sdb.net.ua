6327e44a4368653c6a3685fe8901140487e1d98d
Konst <Konst@DESKTOP-R60G84K>
2024-03-26 12:31:38 +0200

    26.03.2024

--- accountant.php
+++ accountant.php
@@ -846,6 +846,51 @@ if($Auth->isAuth() && isset($_GET['command'])){
                                         }).click();
                                     },
                                     icon: this.icons.open_local
+                                },{
+                                    id: "wdr-tab-cloud",
+                                    title: "Сохранить Отчет",
+                                    handler: (e)=>{
+                                        let html = "<p >Сохранить текущий отчет и отправит на сервер.</p>" +
+                                            "<hr><br>"+
+                                            "<form method='post'>" +
+                                            //"<fieldset>" +
+                                            "<label for='name'>Имя:</label>" +
+                                            "<input type='text' name='name' id='name' class='text ui-widget-content ui-corner-all'>" +
+                                            "<input type='hidden' name='command' value='save-report'>" +
+                                            //"</fieldset>" +
+                                            "</form>";
+                                        $('<div></div>').dialog({
+                                            height: 'auto',
+                                            resizable: false,
+                                            modal: true, title: "Сохранить отчет",
+                                            open: function (){
+                                                $(this).html(html);
+                                                $($(this).find("form")[0]).on('submit',function (e){
+                                                    let form = new FormData(e.currentTarget);
+                                                    const o = Object.fromEntries(form.entries());
+                                                    o.value = pivot.getReport();
+                                                    server.post(o,r=>{
+                                                        if (r.error){
+                                                            ui.alert(r.error);
+                                                        }else
+                                                            ui.alert("OK");
+                                                    },e=>{
+                                                        console.log(e);
+                                                    })
+                                                    $(this).dialog("close");
+                                                    return false;
+                                                }.bind(this))
+                                            },
+                                            buttons: {
+                                                Да: function (){
+                                                    $($(this).find("form")[0]).submit();
+                                                },
+                                                Нет: function (){$(this).dialog("close");}
+                                            }
+                                        });
+                                        console.log("server");
+                                    },
+                                    icon: '<svg xmlns="http://www.w3.org/2000/svg" height="36" viewBox="0 0 640 512"><path d="M537.6 226.6c4.1-10.7 6.4-22.4 6.4-34.6 0-53-43-96-96-96-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32c-88.4 0-160 71.6-160 160 0 2.7 .1 5.4 .2 8.1C40.2 219.8 0 273.2 0 336c0 79.5 64.5 144 144 144h368c70.7 0 128-57.3 128-128 0-61.9-44-113.6-102.4-125.4zM393.4 288H328v112c0 8.8-7.2 16-16 16h-48c-8.8 0-16-7.2-16-16V288h-65.4c-14.3 0-21.4-17.2-11.3-27.3l105.4-105.4c6.2-6.2 16.4-6.2 22.6 0l105.4 105.4c10.1 10.1 2.9 27.3-11.3 27.3z"/></svg>'
                                 });
                                 return tabs;
                             };
@@ -934,48 +979,6 @@ if($Auth->isAuth() && isset($_GET['command'])){
                             }
                         });
                     });
-                    $('.a-report-server').click(e=>{
-                        let html = "<p >Сохранить текущий отчет и отправит на сервер.</p>" +
-                            "<hr><br>"+
-                            "<form method='post'>" +
-                            //"<fieldset>" +
-                            "<label for='name'>Имя:</label>" +
-                            "<input type='text' name='name' id='name' class='text ui-widget-content ui-corner-all'>" +
-                            "<input type='hidden' name='command' value='save-report'>" +
-                            //"</fieldset>" +
-                            "</form>";
-                        $('<div></div>').dialog({
-                            height: 'auto',
-                            resizable: false,
-                            modal: true, title: "Сохранить отчет",
-                            open: function (){
-                                $(this).html(html);
-                                $($(this).find("form")[0]).on('submit',function (e){
-                                    let form = new FormData(e.currentTarget);
-                                    const o = Object.fromEntries(form.entries());
-                                    o.value = pivot.getReport();
-                                    server.post(o,r=>{
-                                        if (r.error){
-                                            ui.alert(r.error);
-                                        }else
-                                            ui.alert("OK");
-                                    },e=>{
-                                        console.log(e);
-                                    })
-                                    $(this).dialog("close");
-                                    return false;
-                                }.bind(this))
-                            },
-                            buttons: {
-                                Да: function (){
-                                    $($(this).find("form")[0]).submit();
-                                    //console.log(form);
-                                },
-                                Нет: function (){$(this).dialog("close");}
-                            }
-                        });
-                        console.log("server");
-                    });
                     $('.a-acc-table').click(e=>{
                         $('.acc-table-box').show();
                     });
@@ -1035,7 +1038,6 @@ if($Auth->isAuth() && isset($_GET['command'])){
                 <?php
                     $user_type = isset($_SESSION['user_data'])?$_SESSION['user_data']->{'type'}:null;
                     if($user_type&&in_array(1,$user_type)){
-                        echo "<div class='a-report-server' title='Сохранить отчет на сервере'><i class='fas fa-cloud-upload-alt'></i></div>";
                         echo "<div class='a-open-reports' title='Отчеты'><i class='fas fa-th-list'></i></div>";
                         echo "<div class='a-acc-table' title='Таблица проводок'><i class='fas fa-table'></i></div>";
                         echo "<div class='a-sub-book' title='Субконто'><i class='fas fa-list-alt'></i></div>";
